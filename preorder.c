883e3a7a4f7dbb89a41c21652a28fd9836c460fd
Yadlapatisreeja <101540053+Yadlapatisreeja@users.noreply.github.com>
2022-05-30 09:49:44 +0530

    Create preorder.c

---
+++ preorder.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node *L;
+    struct node *R;
+};
+struct node* insert( struct node* root, int data ) {
+    if(root == NULL) {
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+        node->data = data;
+        node->L = NULL;
+        node->R = NULL;
+        return node;
+    } else {
+        struct node* cur;
+        if(data <= root->data) {
+            cur = insert(root->L, data);
+            root->L = cur;
+        } else {
+            cur = insert(root->R, data);
+            root->R= cur;
+        }
+        return root;
+    }
+}
+void preOrder( struct node *root) {
+if (root != NULL) {
+        printf("%d ", root->data);
+        preOrder(root->L);
+        preOrder(root->R);
+    }
+}
+int main() {
+    struct node* root = NULL;
+    int t;
+    int data;
+    scanf("%d", &t);
+    while(t-- > 0) {
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+    preOrder(root);
+    return 0;
+}
