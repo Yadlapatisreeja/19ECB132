84dd678eac1e29dfd89ff1b38325d8bd74fdb37c
SREEJA YADLAPATI YADLAPATI <101540053+Yadlapatisreeja@users.noreply.github.com>
2022-06-01 03:59:46 +0000

    .

--- height_of_tree.c
+++ height_of_tree.c
@@ -1,62 +1,46 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
 struct node {
     int data;
-    struct node *L;
-    struct node *R;
+    struct node* left;
+    struct node* right;
 };
-struct node* insert( struct node* root, int data ) {
-    if(root == NULL) {
-        struct node* node = (struct node*)malloc(sizeof(struct node));
-        node->data = data;
-        node->L = NULL;
-        node->R = NULL;
-        return node;
-    } else {
-        struct node* cur;
-        if(data <= root->data) {
-            cur = insert(root->L, data);
-            root->L= cur;
-        } else {
-            cur = insert(root->R, data);
-            root->R = cur;
-        }
-        return root;
-    }
-}
-int getHeight(struct node* root)
+int maxDepth(struct node* node)
 {
-
-int counter_R = 0;
-int counter_L = 0;   
-    if (root == NULL)
-    {
+    if (node == NULL)
         return -1;
+    else {
+        /* compute the depth of each subtree */
+        int lDepth = maxDepth(node->left);
+        int rDepth = maxDepth(node->right);
+ 
+        /* use the larger one */
+        if (lDepth > rDepth)
+            return (lDepth + 1);
+        else
+            return (rDepth + 1);
     }
-         counter_L = getHeight(root->L);
-        counter_L++;
-         
-         counter_R = getHeight(root->R);
-         counter_R++; 
-         
-        if (counter_L > counter_R)
-        {
-            return counter_L;
-        }
-              
-         return counter_R;     
 }
-int main() {
-    struct node* root = NULL;
-    int t;
-    int data;
-    scanf("%d", &t);
-    while(t-- > 0) {
-        scanf("%d", &data);
-        root = insert(root, data);
-    }
-    printf("%d",getHeight(root));
+
+struct node* newNode(int data)
+{
+    struct node* node
+        = (struct node*)malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
+ 
+int main()
+{
+    struct node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    printf("Height of tree is %d", maxDepth(root));
+    getchar();
     return 0;
 }
+/*OUTPUT:Height of tree is 2*/
