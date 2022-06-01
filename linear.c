22ec56337da56660ea4b5dc420bdb470104f9236
SREEJA YADLAPATI YADLAPATI <101540053+Yadlapatisreeja@users.noreply.github.com>
2022-06-01 03:52:19 +0000

    output added

--- linear.c
+++ linear.c
@@ -130,3 +130,23 @@ int main(void) {
     
   }
 }
+
+/*output:
+1. Stack using Array
+2. Queue using Array
+3. Stack using Linked List
+4. Queue using Linked List
+5. Exit from the program
+
+ Provide an option: 1
+
+***Creation of Stack using Array***
+ 1. Push     2. Pop      3. Display      4. Return to previous menu 
+ Choose an option: 1
+
+Enter the element to be pushed to the stack: 56
+
+ 1. Push     2. Pop      3. Display      4. Return to previous menu 
+ Choose an option: 2
+
+Popped element is: 56*/
